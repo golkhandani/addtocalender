6c85ec82ecc0e2485510ea2690cf09e060d5258f
Golkhandani <golkhandani@gmail.com>
2025-06-30 03:46:50 -0700

    fix: cors

--- backend/main.go
+++ backend/main.go
@@ -51,7 +51,7 @@ type EventData struct {
 
 func useCors(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "https://addtocalender-1.onrender.com")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
